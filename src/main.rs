101167ead8b1bc734ea49dc163102f1d9300d56c
Mike Hewner <hewner@rose-hulman.edu>
2022-08-01 09:01:58 -0400

    about to try tokio

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,83 @@
 use futures::executor::block_on;
 use futures::join;
-use async_timer::oneshot::{ Timer, Oneshot};
 use std::error::Error;
-use std::time::Duration;
-use console::Term;
-use std::io::Write;
 use awaitplay::{SimpleEngine, Engine};
-
-fn main()  -> Result<(), Box<dyn Error>> {
-
+use std::ops::Add;
 
 
-    let future = async {
-
-        let mut engine = SimpleEngine::new();
-        
-        
-        for i in 0..30 {
-            engine.draw_glyph('?', 0, i).await;
-            engine.wait(1./6.).await;
-        }
-        
-    };
+use rand::Rng;
+use rand::thread_rng;
 
 
-    let future2 = async {
+#[derive(Copy, Clone)]
+struct Pos {
+    row : i16,
+    col : i16
+}
 
-        let mut engine = SimpleEngine::new();
+fn pos(r : i16, c : i16) -> Pos {
+    Pos { row : r, col : c }
+}
 
-        
-        for i in 0..5 {
-            engine.draw_glyph('?', i, 0).await;
-            engine.wait(1.).await;
-        }
-        
-    };
+
+impl Add<Pos> for Pos {
+    type Output = Pos;
+    fn add(self, other : Pos) -> Pos {
+        Pos { row: self.row + other.row, col : self.col + other.col }
+    }
+}
+
+fn is_valid(cur: &Pos) -> bool {
+    (cur.row >= 0) & (cur.row < 80) & (cur.col >= 0) & (cur.col < 80)
+}
+
+
+async fn moveX() {
+
+    let mut rng = thread_rng();
+    
+    let mut engine = SimpleEngine::new();
+    let row_start: i16 = rng.gen_range(0..40);
+    let col_start: i16 = rng.gen_range(0..80);
+
+    let mut start = pos(row_start,col_start);
+    for i in 0..10 {
+        start = moveStep(&mut engine, start).await;
+        if !is_valid(&start) { break; }
+    }
+    
+}
+
+
+async fn moveStep(engine : &mut SimpleEngine, mut cur : Pos) -> Pos {
+
+
 
     
+    let mut rng = thread_rng();
 
-    let all = async { join!(future, future2); };
+    let dir: usize = rng.gen_range(0..4);
+    let deltas = vec![pos(0,1), pos(1,0), pos(0,-1), pos(-1,0)];
+
+
+    
+    
+    for _i in 0..5 {
+
+        engine.draw_glyph(' ', cur.row.try_into().unwrap(), cur.col.try_into().unwrap()).await;
+        cur = cur + deltas[dir];
+        if !is_valid(&cur) { return cur };
+        engine.draw_glyph('X', cur.row.try_into().unwrap(), cur.col.try_into().unwrap()).await;
+        engine.wait(0.1).await;
+    }
+    cur
+}
+
+
+fn main()  -> Result<(), Box<dyn Error>> {
+    
+
+    let all = async { join!(moveX(), moveX(), moveX(), moveX(), moveX(), moveX(), moveX(), moveX(), moveX(), moveX(), moveX(), moveX(), moveX(), moveX(), moveX(), moveX(), moveX(), moveX(), moveX()); };
 
     block_on(all);
     Ok(())
